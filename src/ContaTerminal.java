b9e066a2464fa26976f278dab44f520c75d30008
Maurício Galdino <mauricio-galdino@hotmail.com>
2024-06-08 13:39:41 -0300

    desafio ContaTerminal

---
+++ src/ContaTerminal.java
@@ -0,0 +1,23 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class ContaTerminal {
+    public static void main(String[] args) throws Exception {
+        //criando o objeto scanner
+        Scanner scanner = new Scanner(System.in).useDelimiter("\\n").useLocale(Locale.US);
+        
+        System.out.println("Por favor, digite o número da conta:");
+        int numeroConta = scanner.nextInt();
+        
+        System.out.println("Por favor, digite o número da Agência:");
+        String numeroAgencia = scanner.next();
+
+        System.out.println("Por favor, digite seu nome:");
+        String nomeCliente = scanner.next();
+        
+        System.out.println("Por favor, digite seu saldo:");
+        double saldoCliente = scanner.nextDouble();
+
+        System.out.println("Olá "+ nomeCliente +", obrigado por criar uma conta em nosso banco, sua agência é "+ numeroAgencia +", conta "+ numeroConta +" e seu saldo "+ saldoCliente +" já está disponível para saque.");
+    }
+}
