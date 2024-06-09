c4c54c4918fa659413063990f79c725b9f63390c
Maurício Galdino <mauricio-galdino@hotmail.com>
2024-06-09 13:11:32 -0300

    adicionando tratamento de error

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -1,3 +1,4 @@
+import java.util.InputMismatchException;
 import java.util.Locale;
 import java.util.Scanner;
 
@@ -5,19 +6,26 @@ public class ContaTerminal {
     public static void main(String[] args) throws Exception {
         //criando o objeto scanner
         Scanner scanner = new Scanner(System.in).useDelimiter("\\n").useLocale(Locale.US);
-        
-        System.out.println("Por favor, digite o número da conta:");
-        int numeroConta = scanner.nextInt();
-        
-        System.out.println("Por favor, digite o número da Agência:");
-        String numeroAgencia = scanner.next();
 
-        System.out.println("Por favor, digite seu nome:");
-        String nomeCliente = scanner.next();
-        
-        System.out.println("Por favor, digite seu saldo:");
-        double saldoCliente = scanner.nextDouble();
+        try {
+            System.out.println("Por favor, digite o número da conta:");
+            int numeroConta = scanner.nextInt();
 
-        System.out.println("Olá "+ nomeCliente +", obrigado por criar uma conta em nosso banco, sua agência é "+ numeroAgencia +", conta "+ numeroConta +" e seu saldo "+ saldoCliente +" já está disponível para saque.");
+            System.out.println("Por favor, digite o número da Agência:");
+            String numeroAgencia = scanner.next();
+
+            System.out.println("Por favor, digite seu nome:");
+            String nomeCliente = scanner.next();
+
+            System.out.println("Por favor, digite seu saldo:");
+            double saldoCliente = scanner.nextDouble();
+
+            System.out.println("Olá "+ nomeCliente +", obrigado por criar uma conta em nosso banco, sua agência é "+ numeroAgencia +", conta "+ numeroConta +" e seu saldo "+ saldoCliente +" já está disponível para saque.");
+
+        } catch (InputMismatchException e) {
+            System.out.println("Os campos número da conta e saldo da conta precisam ser numéricos!");
+        } finally {
+            scanner.close();
+        }
     }
 }
